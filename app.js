9e6cdfcf9cb759a4ed6777da2f9b935e8fcfd568
Yoav <yoav.benzvi@gmail.com>
2020-05-30 14:25:44 +0200

    added basic get routes for places
    -added routes for finding places with user id and place id
    -currently using an array as a fake database

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 const express = require('express');
-
 const app = express();
 
+const placesRoutes = require('./routes/places-routes') ;
 
+app.use('/api/places', placesRoutes);
 
 app.listen(5000, () => {
 	'app is running on port 5000'
---
+++ routes/places-routes.js
@@ -0,0 +1,33 @@
+const express = require('express');
+
+const router = express.Router();
+
+const DUMMY_PLACES = [
+	{
+		id: 'p1',
+		title: 'Empire state building',
+		description: 'a famous skyscraper',
+		location: {
+			lat: 40.7484474,
+			lng: -73.9871516
+		},
+		address: '20 W 34th St. New York, NY 10001',
+		creator: 'u1'
+	}
+];
+
+router.get('/:pid', (req, res, next) => {
+	const placeId = req.params.pid;
+	const place = DUMMY_PLACES.find(p => p.id === placeId)
+	res.json({place})
+});
+
+router.get('/user/:uid', (req, res, next) => {
+	const userId = req.params.uid;
+
+	const place = DUMMY_PLACES.find(p => p.creator === userId);
+
+	res.json({place})
+})
+
+module.exports = router;
