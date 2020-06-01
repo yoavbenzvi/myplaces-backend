f1bb52c849db1f854ff6971ebcc674cb7b00f3d0
Yoav <yoav.benzvi@gmail.com>
2020-06-01 18:47:08 +0200

    added validation to routes
    -installed express-validator package
    -added validation to following routes:
     create place
     update place
     delete place
     signup

--- routes/users-routes.js
+++ routes/users-routes.js
@@ -1,10 +1,18 @@
 const express = require('express');
+const { check } = require('express-validator');
 const usersControllers = require('../controllers/users-controllers');
 
 const router = express.Router();
 
 router.get('/', usersControllers.getUsers);
-router.post('/signup', usersControllers.signup);
+
+router.post('/signup', 
+	[
+		check('name').not().isEmpty(),
+		check('email').normalizeEmail().isEmail(),
+		check('password').isLength({min: 4}) //Change later to longer
+	], usersControllers.signup);
+
 router.post('/login', usersControllers.login);
 
 module.exports = router;
