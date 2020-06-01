d796aa0896ea59cec1a214c7608fa49a8991b19c
Yoav <yoav.benzvi@gmail.com>
2020-06-01 16:05:34 +0200

    added errors
    -added a HttpError class to keep code DRY
    -added middleware for error handling
    -added error handling for places routes

--- routes/places-routes.js
+++ routes/places-routes.js
@@ -1,5 +1,5 @@
 const express = require('express');
-
+const HttpError = require('../models/http-error'); 
 const router = express.Router();
 
 const DUMMY_PLACES = [
@@ -18,7 +18,13 @@ const DUMMY_PLACES = [
 
 router.get('/:pid', (req, res, next) => {
 	const placeId = req.params.pid;
+
 	const place = DUMMY_PLACES.find(p => p.id === placeId)
+
+	if(!place) {
+		throw new HttpError('Could not find place for the provided id', 404)
+	}
+
 	res.json({place})
 });
 
@@ -26,6 +32,10 @@ router.get('/user/:uid', (req, res, next) => {
 	const userId = req.params.uid;
 
 	const place = DUMMY_PLACES.find(p => p.creator === userId);
+
+	if(!place) {
+		return next(new HttpError('Could not find place for the provided user id', 404));
+	}
 
 	res.json({place})
 })
