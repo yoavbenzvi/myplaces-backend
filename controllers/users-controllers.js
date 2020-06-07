5e51807c7a40d0e4b343878dc7f7fc214ff01dc9
Yoav <yoav.benzvi@gmail.com>
2020-06-07 19:10:39 +0200

    added hashing
    - installed bcryptjs package
    - added password hashing and comparing with bcrypt

--- controllers/users-controllers.js
+++ controllers/users-controllers.js
@@ -1,4 +1,5 @@
 const { validationResult } = require('express-validator');
+const bcrypt = require('bcryptjs');
 const HttpError = require('../models/http-error');
 const User = require('../models/user');
 
@@ -35,11 +36,19 @@ const signup = async (req, res, next) => {
 		return next(error);
 	}
 
+	let hashedPassword;
+	try {
+		const hashedPassword = await bcrypt.hash(password, 12);
+	} catch(err) {
+		const error = new HttpError('Could not create user, please try again', 500)
+		return next(error);
+	}
+
 	const createdUser = new User({
 		name,
 		email,
 		image: req.file.path,
-		password,
+		password: hashedPassword,
 		places: []
 	});
 
@@ -64,7 +73,20 @@ const login = async (req, res, next) => {
 		return next(error);
 	}
 
-	if(!existingUser || existingUser.password !== password) {
+	if(!existingUser) {
+		const error = new HttpError('Invalid credentials. Please check credentials or try again later', 401)
+		return next(error);
+	}
+
+	let isValidPassword = false;
+	try {
+		isValidPassword = await bcrypt.compare(password, existingUser.password)
+	} catch(err) {
+		const error = new HttpError('Could not log in. Please check credentials', 500)
+		return next(error);
+	}
+
+	if(!isValidPassword) {
 		const error = new HttpError('Invalid credentials. Please check credentials or try again later', 401)
 		return next(error);
 	}
